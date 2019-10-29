e9626bfc9b12745d7f93e693a6c875559870ad57
Anthony Hanson <ahanson@udallas.edu>
2019-10-29 07:28:36 -0700

    Set up main story board

---
+++ Homework6/Homework6/Switcher.swift
@@ -0,0 +1,9 @@
+//
+//  Switcher.swift
+//  Homework6
+//
+//  Created by Anthony Hanson on 10/29/19.
+//  Copyright © 2019 Anthony Hanson. All rights reserved.
+//
+
+import Foundation
--- Homework6/Homework6/ViewController.swift
+++ Homework6/Homework6/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        button.isSelected.toggle()
+    }
 }
 
